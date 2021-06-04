812efe8e3e57c7b5766728c3be4235c0928cb259
Cipriano Armijo <>
2021-06-04 00:12:15 +0000

    removed let from input.question

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,13 +15,13 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  let candidateName = input.question('What is your name? ');
+  candidateName = input.question('What is your name? ');
   //console.log("Hello " + candidateName);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  let candidateAnswer = input.question(question)
+  candidateAnswer = input.question(question)
 }
 
 function gradeQuiz(candidateAnswers) {
