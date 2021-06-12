0b78780eccc8de06d913dd29db196f54a0498e7d
Cipriano Armijo <80788339+CiprianoArmijo@users.noreply.github.com>
2021-06-12 16:17:17 +0000

    Added grading

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,12 +5,15 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let questions = ["Who was the first American woman in space? ", "true or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+let questions = ["1) Who was the first American woman in space? ", "2) true or false: 5 kilometer == 5000 meters? ", "3) (5 + 3)/2 * 10 = ? ", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 let question;
 let correctAnswer;
 let candidateAnswer;
+let passFail = '';
+let grade = 0;
+let counter = 0;
 
 
 function askForName() {
@@ -29,27 +32,27 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  console.log(candidateAnswers);
+  //console.log(candidateAnswers);
+  console.log(`Candidate Name: ${candidateName}`);
   for (let i = 0; i < questions.length; i++) {
-    console.log(`The question: "${questions[i]}" Had an answer of "${correctAnswers[i]}"`);
-    if(candidateAnswers[i] != correctAnswers[i]) {
-      console.log(` You answered: ${candidateAnswers[i]} " Incorrect!`)
+    console.log(`${questions[i]} 
+    Your answer: ${candidateAnswers[i]}
+    Correct Answer: ${correctAnswers[i]}`);
+  
+
+    if(candidateAnswers[i] === correctAnswers[i]) {
+       grade += 20;
+      counter += 1;
+      if(grade >= 80) {
+        passFail = 'PASSED!';
+      } else {
+        passFail = 'FAILED';
+      }
     } 
-  }/*else
-      console.log(" Correct!");
   }
+  console.log(`>>> Overall Grade: ${grade}% (${counter} of 5 responses correct <<<
+    >>> Status: ${passFail} <<<`);
   
-  if (candidateAnswers === correctAnswers) {
-    console.log("Correct!")
-  } else {
-    console.log("Incorrect!")
-  }*/
-
-
-
-  let grade;
-  
-
   return grade;
 }
 
