d77f95fa7859f73c47c67f49aaf8c63316d42758
Cipriano Armijo <80788339+CiprianoArmijo@users.noreply.github.com>
2021-06-12 16:47:14 +0000

    questions format

--- candidate-testing.js
+++ candidate-testing.js
@@ -5,7 +5,7 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = "";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let questions = [ "1) Who was the first American woman in space? ", "2) True or false: 5 kilometers == 5000 meters? ", "3) (5 + 3)/2 * 10 = ? ", "4) Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "5) What is the minimum crew size for the ISS? "];
+let questions = [ "Who was the first American woman in space? ", "True or false: 5 kilometers == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 let question = "";
@@ -52,6 +52,7 @@ function gradeQuiz(candidateAnswers) {
   }
   console.log(`>>> Overall Grade: ${grade}% (${counter} of 5 responses correct <<<
     >>> Status: ${passFail} <<<`);
+    console.log(typeof grade);
   
   return grade;
 }
